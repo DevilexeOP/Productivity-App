eaa01584c6eee6e1d9c2910b9b3c311fa033e05e
thakkarnetram <thakkarnetram10@gmail.com>
2023-10-04 17:36:52 +0530

    dev-tested-checked

--- src/Config/constants.js
+++ src/Config/constants.js
@@ -4,4 +4,4 @@
 // export const ROOT_URL = 'http://172.22.128.1:8082';
 // export const ROOT_URL = 'http://192.168.29.154:8082'; // HOME PC
 // export const ROOT_URL = 'http://192.168.1.8:8082';
-export const ROOT_URL = 'http://192.168.1.10:8082';
+export const ROOT_URL = 'http://192.168.1.5:8082';
